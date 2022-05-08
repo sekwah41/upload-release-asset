364c28cfecb0bf21b29ba74a70ec6d169081ce75
Sekwah <contact@sekwah.com>
2022-05-08 05:13:56 +0100

    feat: add main upload and default prop logic

--- src/upload-release-asset.js
+++ src/upload-release-asset.js
@@ -2,7 +2,9 @@ const core = require('@actions/core');
 const glob = require('@actions/glob');
 const { GitHub } = require('@actions/github');
 const fs = require('fs');
+const path = require('path');
 const fsPromises = fs.promises;
+const mime = require('mime-types');
 
 const { debug, info, warning } = core;
 
@@ -14,12 +16,22 @@ function getDefaultGlobOptions() {
   };
 }
 
-async function uploadFile(github, uploadUrl, assetPath, assetName, assetContentType) {
+/**
+ * @param github
+ * @param uploadUrl
+ * @param assetPath
+ * @param assetContentType
+ * @param assetName`
+ * @returns {Promise<*>}
+ */
+async function uploadFile({ github, uploadUrl, assetPath, assetContentType, assetName }) {
   // Determine content-length for header to upload asset
   const contentLength = filePath => fs.statSync(filePath).size;
+  const mediaType = assetContentType ? assetContentType : mime.lookup(path.extname(assetPath));
+  const fileName = assetName ? assetName : path.basename(assetPath);
 
   // Setup headers for API call, see Octokit Documentation: https://octokit.github.io/rest.js/#octokit-routes-repos-upload-release-asset for more information
-  const headers = { 'content-type': assetContentType, 'content-length': contentLength(assetPath) };
+  const headers = { 'content-type': mediaType, 'content-length': contentLength(assetPath) };
 
   // Upload a release asset
   // API Documentation: https://developer.github.com/v3/repos/releases/#upload-a-release-asset
@@ -27,7 +39,7 @@ async function uploadFile(github, uploadUrl, assetPath, assetName, assetContentT
   const uploadAssetResponse = await github.repos.uploadReleaseAsset({
     url: uploadUrl,
     headers,
-    name: assetName,
+    name: fileName,
     file: fs.readFileSync(assetPath)
   });
 
@@ -80,19 +92,42 @@ async function run() {
     // Get the inputs from the workflow file: https://github.com/actions/toolkit/tree/master/packages/core#inputsoutputs
     const uploadUrl = core.getInput('upload_url', { required: true });
     const assetPath = core.getInput('asset_path', { required: true });
-    const assetName = core.getInput('asset_name', { required: true });
+    const assetName = core.getInput('asset_name', { required: false });
     const assetContentType = core.getInput('asset_content_type', { required: false });
 
     const searchResults = await filesToUpload(assetPath);
 
+    const downloadUrls = [];
+
     if (searchResults.length === 0) {
       warning(`No files were found with the provided path: ${assetPath}. No assets will be uploaded.`);
+    } else if (searchResults.length === 1) {
+      info(`Found 1 file to upload: ${searchResults[0]}`);
+      downloadUrls.push(
+        await uploadFile({
+          github,
+          uploadUrl,
+          assetPath: searchResults[0],
+          assetContentType,
+          assetName
+        })
+      );
+    } else {
+      info(`Found ${searchResults.length} files to upload`);
+      for (const file of searchResults) {
+        downloadUrls.push(
+          await uploadFile({
+            github,
+            uploadUrl,
+            assetPath: file,
+            assetContentType
+          })
+        );
+      }
     }
 
-    const browserDownloadUrl = await uploadFile(github, uploadUrl, assetPath, assetName, assetContentType);
-
     // Set the output variable for use by other actions: https://github.com/actions/toolkit/tree/master/packages/core#inputsoutputs
-    core.setOutput('browser_download_url', [browserDownloadUrl]);
+    core.setOutput('browser_download_url', downloadUrls);
   } catch (error) {
     core.setFailed(error.message);
   }
